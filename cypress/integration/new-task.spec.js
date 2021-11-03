6529a71e7a9482e2bb5f2bcbf428e73e6308c1c0
Bernard Mordan <bernardmordan@gmail.com>
2021-11-03 16:23:34 +0000

    remove cypress examples, adds npm e2e scripts

--- cypress/integration/new-task.spec.js
+++ cypress/integration/new-task.spec.js
@@ -1,8 +1,6 @@
-const { expect } = require("chai")
-
 describe('Adding Users', () => {
     it('has a modal opened by an add task button', () => {
-        cy.visit('http://localhost:3000')
+        cy.visit('http://localhost:3001')
         cy.get('.user-list button').click()
         cy.get('#modal').contains('Add User')
     })
